27af0d6ed51b1fc78c291d3eed72b4d445fe5e24
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-12 20:55:43 +0100

    codecrafters submit [skip ci]

--- app/processor.py
+++ app/processor.py
@@ -1,4 +1,5 @@
 END_LINE = b"\r\n"
+ECHO_PATH = b"/echo/"
 
 
 def processor(request: bytes) -> bytes:
@@ -9,5 +10,9 @@ def processor(request: bytes) -> bytes:
         return b"TODO"
     if request_target == b"/":
         return b"HTTP/1.1 200 OK\r\n\r\n"
+    elif request_target.startswith(ECHO_PATH):
+        echo_text = request_target[len(ECHO_PATH) :]
+        result = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_text)}\r\n\r\n"
+        return result.encode() + echo_text
     else:
         return b"HTTP/1.1 404 Not Found\r\n\r\n"
