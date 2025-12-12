fd259c199d5864c4301dd7eddb24b505f549fc29
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-12-12 20:51:39 +0100

    codecrafters submit [skip ci]

--- app/processor.py
+++ app/processor.py
@@ -1,2 +1,13 @@
+END_LINE = b"\r\n"
+
+
 def processor(request: bytes) -> bytes:
-    return b"HTTP/1.1 200 OK\r\n\r\n"
+    lines = request.split(END_LINE)
+    try:
+        request_target = lines[0].split(b" ")[1]
+    except IndexError:
+        return b"TODO"
+    if request_target == b"/":
+        return b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        return b"HTTP/1.1 404 Not Found\r\n\r\n"
